39d23f595a6e388336aa9dbb23acdcb8cfe3a595
jiawei-hong <zxc37984@gmail.com>
2021-12-22 17:48:20 +0800

    feat:add DBConfig use in App Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Calendar from "./components/Calendar";
+import { IndexedDB, initDB } from './indexed-db';
+import { DBConfig } from './DBConfig';
+
+initDB(DBConfig);
 
 function App() {
   return (
-    <div className="App">
+    <IndexedDB>
       <Calendar />
-    </div>
+    </IndexedDB>
   );
 }
 
